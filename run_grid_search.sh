a98299e0977e208ccd78cf24a389c6d37a987f79
lewinsda <lewinsda@metal-1-0.local>
2022-08-09 09:25:21 -0700

    grid search analysis

--- run_grid_search.sh
+++ run_grid_search.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --mem=8g
-#SBATCH --array=1-1000%100
+#SBATCH --array=1-540%100
 #SBATCH --partition=gpu
 #SBATCH --time=30
 #SBATCH --output=log-%j.out
 #SBATCH --error=log-%j.err
 #SBATCH --gres=gpu:2
 
-python -u test_config.py /home/groups/ConradLab/daniel/sharp_sims/splat_0.7_de_rq/ grid_search_0.7_output/ ${SLURM_ARRAY_TASK_ID}.txt
+python -u test_config.py /home/groups/ConradLab/daniel/sharp_sims/splat_0.6_de_rq/ grid_search_0.6_output/ ${SLURM_ARRAY_TASK_ID}.txt
