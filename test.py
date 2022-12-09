5682ea88f8327f1ac2de5364ad930f8f5ee2538f
bwmodlin <b_modlin@coloradocollege.edu>
2022-12-09 10:33:31 -0700

    Test file path update

--- test.py
+++ test.py
@@ -7,5 +7,9 @@ out_path = "./output"
 marker_path = "simulations/splat_0.7/markers.txt"
 neighbors=2
 config="configs/2_25.txt"
+ref_path = "examples/splat_0.8/ref_counts.csv.gz"
+ref_label_path = "examples/splat_0.8/ref_labels.csv"
 
 sharp = scSHARP(data_path, tool_list, marker_path, neighbors, config)
+
+scSHARP.run_tools(out_path, ref_path, ref_label_path)
