26c104910e20073af03c2b717c6c821c433ad7d3
lewinsda <lewinsda@metal-1-0.local>
2023-03-11 17:03:29 -0800

    de updates

--- run_de.R
+++ run_de.R
@@ -7,5 +7,5 @@ data_path <- args[1]
 data <- readRDS(paste(data_path,"process_seur.rds",sep=""))
 Idents(data) <- "sharp_preds"
 print(dim(data))
-data.markers <- FindAllMarkers(data, only.pos = TRUE, verbose=TRUE)
+data.markers <- FindAllMarkers(data, only.pos = TRUE, verbose=TRUE, random.seed=1)
 write.csv(data.markers, paste(data_path, "de_genes.csv",sep=""))
