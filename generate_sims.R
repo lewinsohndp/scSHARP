1ae785e523311a164dcc53073d1d3cff693840b5
lewinsda <lewinsda@metal-1-0.local>
2023-03-12 19:26:15 -0700

    simulation generation

--- generate_sims.R
+++ generate_sims.R
@@ -14,7 +14,7 @@ run_analysis <- function(seur_obj){
   return(seur_obj)
 }
 
-folder <- "/home/groups/ConradLab/daniel/sharp_data/sharp_sims_de_testing/"
+folder <- "/home/groups/ConradLab/daniel/sharp_data/sharp_sims_test/"
 param_data_path <- "~/scSHARP/filtered_gene_bc_matrices/GRCh38/matrix.mtx"
 # folder <- "~/desktop/conradlab/thesis/scsharp/simulations/"
 # param_data_path <- "~/desktop/conradlab/thesis/scSHARP/filtered_gene_bc_matrices/GRCh38/matrix.mtx"
@@ -27,10 +27,10 @@ set.seed(8)
 
 params <- splatEstimate(as.matrix(counts))
 
-for(i in seq(.3,1.0,length.out=8)){
-  full_path <- paste(folder,"splat_",i,"_de_rq_103122/", sep="")
+for(i in seq(.7,.8,length.out=2)){
+  full_path <- paste(folder,"splat_",i,"_de_rq_imbalance_031223/", sep="")
   dir.create(full_path)
-  params <- setParams(params, dropout.type='experiment', batchCells=c(1000,1000), batch.facScale=0.5, group.prob = c(.25,.25,.25,.25), de.facScale=i, seed=888)
+  params <- setParams(params, dropout.type='experiment', batchCells=c(1000,1000), batch.facScale=0.5, group.prob = c(.4,.3,.2,.1), de.facScale=i, seed=888)
   print(params)
   sim.groups <- splatSimulate(params, method = "groups", verbose = T)
   
